772d507409334264f32c3006639e50530ead2fdd
Jorge Ramírez <jorgeramirez1990@gmail.com>
2024-12-27 14:25:29 -0300

    add github icon to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,40 @@
 import React from "react";
-import logoFooter from "../assets/imagenes/logo-footer.png"
-import linkedinLogo from "../assets/imagenes/in.png"
-import mailLogo from "../assets/imagenes/mail.png"
+import logoFooter from "../assets/imagenes/logo-footer.png";
+import linkedinLogo from "../assets/imagenes/in.png";
+import mailLogo from "../assets/imagenes/mail.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faGithub);
 
 export default function Footer() {
   return (
     <footer className="bg-[#063231] text-center text-white py-16 flex flex-col items-center">
       <div className="flex flex-col items-center">
         <div className="w-72 h-72 flex items-center justify-center mb-6">
-          <img src={logoFooter} alt="Sofia Morales" className="rounded-lg w-auto h-auto" />
+          <img
+            src={logoFooter}
+            alt="Sofia Morales"
+            className="rounded-lg w-auto h-auto"
+          />
         </div>
       </div>
-      <h3 className="text-3xl font-semibold mb-6 text-[#F9F5E3]">Thanks for stopping by!</h3>
+      <h3 className="text-3xl font-semibold mb-6 text-[#F9F5E3]">
+        Thanks for stopping by!
+      </h3>
       <div className="flex gap-4 items-center">
-        <img src={mailLogo} className="flex items-center justify-center w-auto h-[50px]" />
-        <img src={linkedinLogo} className="flex items-center justify-center w-auto h-[40px]" />
+        <img
+          src={mailLogo}
+          className="flex items-center justify-center w-auto h-[50px]"
+        />
+        <img
+          src={linkedinLogo}
+          className="flex items-center justify-center w-auto h-[40px]"
+        />
+        <a href="https://github.com/somorales" target="_blank">
+          <FontAwesomeIcon className="h-[42px] text-[#F9F5E3]" icon="fa-brands fa-github" />
+        </a>
       </div>
     </footer>
   );
